c58824a2c7fc87e8f4d44cbc06d1ca072cc5e5d2
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-24 12:41:04 -0700

    switch to pull weather from server

---
+++ functions/getWeather.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const serverlessHttp = require('serverless-http');
+const axios = require('axios');
+
+const app = express();
+
+app.get('/.netlify/functions/getWeather', async (req, res) => {
+  try {
+    const { lat, lon } = req.query;
+
+    const weatherEndpoint = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHERBIT_API_KEY}`;
+    const response = await axios.get(weatherEndpoint);
+
+    if (response.status !== 200) {
+      res.status(500).json({ message: 'Failed to fetch weather data.' });
+      return;
+    }
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: error.toString() });
+  }
+});
+
+module.exports.handler = serverlessHttp(app);
