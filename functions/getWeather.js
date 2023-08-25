9412936874149b1691e7dc5e557b648831e0775a
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-24 21:20:58 -0700

    class09

--- functions/getWeather.js
+++ functions/getWeather.js
@@ -7,6 +7,8 @@ const app = express();
 app.get('/.netlify/functions/getWeather', async (req, res) => {
   try {
     const { lat, lon } = req.query;
+ // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', 'https://magenta-stardust-08f1b6.netlify.app');
 
     const weatherEndpoint = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_WEATHERBIT_API_KEY}`;
     const response = await axios.get(weatherEndpoint);
